8134bc81e92b4b235c2e2fcaa52f6e855e108487
Hyungchan Kim <inlinechan@gmail.com>
2015-04-22 09:31:01 +0900

    Move method into inside of class as @staticmethod

--- bin/test_websocket_server.py
+++ bin/test_websocket_server.py
@@ -17,34 +17,36 @@ except ImportError:
     import trollius as asyncio
     from trollius import From
 
-@asyncio.coroutine
-def sendLine():
-    pendings = []
-    while True:
-        line = yield From(queue.get())
-        print("Queue.get: {}".format(line))
-        for conn in CONNECTIONS:
-            if not hasattr(conn, 'id'):
-                setattr(conn, 'id', 1)
-
-            if not hasattr(conn, 'table_id') and not hasattr(conn, 'waitingTableId'):
-                msg = {'jsonrpc': '2.0', 'id': conn.id, 'method': 'create_table',
-                       'params': line.split()}
-                setattr(conn, 'waitingTableId', True)
-            else:
-                msg = {'jsonrpc': '2.0', 'id': conn.id, 'method': 'append_row',
-                       'table_id': conn.table_id, 'params': line.split()}
-
-            conn.id += 1
-            conn.sendMessage(json.dumps(msg).encode('utf8'))
-            print("Sending msg: {}".format(msg))
-
-def handleStdin(input):
-    data = input.readline().strip()
-    print("Queue.put: {}".format(data))
-    asyncio.async(queue.put(data)) # Queue.put is a coroutine, so you can't call it directly.
-
 class MyServerProtocol(WebSocketServerProtocol):
+    @staticmethod
+    def handleStdin(input):
+        data = input.readline().strip()
+        print("Queue.put: {}".format(data))
+        asyncio.async(queue.put(data)) # Queue.put is a coroutine, so you can't call it directly.
+
+    @staticmethod
+    @asyncio.coroutine
+    def sendLine():
+        while True:
+            line = yield From(queue.get())
+            print("Queue.get: {}".format(line))
+            for conn in CONNECTIONS:
+                if not hasattr(conn, 'id'):
+                    setattr(conn, 'id', 1)
+
+                if not hasattr(conn, 'table_id') and not hasattr(conn, 'waitingTableId'):
+                    msg = {'jsonrpc': '2.0', 'id': conn.id, 'method': 'create_table',
+                           'params': line.split()}
+                    setattr(conn, 'waitingTableId', True)
+                else:
+                    msg = {'jsonrpc': '2.0', 'id': conn.id, 'method': 'append_row',
+                           'table_id': conn.table_id, 'params': line.split()}
+
+                conn.id += 1
+                conn.sendMessage(json.dumps(msg).encode('utf8'))
+                print("Sending msg: {}".format(msg))
+
+
     def onConnect(self, request):
         print("Client connecting: {0}".format(request.peer))
 
@@ -52,7 +54,7 @@ class MyServerProtocol(WebSocketServerProtocol):
         print("WebSocket connection open.")
         CONNECTIONS.append(self)
         loop = asyncio.get_event_loop()
-        loop.add_reader(sys.stdin, functools.partial(handleStdin, sys.stdin))
+        loop.add_reader(sys.stdin, functools.partial(MyServerProtocol.handleStdin, sys.stdin))
 
     def onMessage(self, payload, isBinary):
         print("Received: {}".format(payload))
@@ -86,7 +88,7 @@ if __name__ == '__main__':
     # loop.add_reader(sys.stdin, functools.partial(handleStdin, sys.stdin))
     coro = loop.create_server(factory, '127.0.0.1', int(PORT))
     server = loop.run_until_complete(asyncio.wait(
-        [coro, asyncio.async(sendLine())]))
+        [coro, asyncio.async(MyServerProtocol.sendLine())]))
 
     try:
         loop.run_forever()
