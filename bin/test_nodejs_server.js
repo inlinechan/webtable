53b2cf4d51c8f85faa8313b7fc49b63dd368c0c2
Hyungchan Kim <inlinechan@gmail.com>
2015-05-24 16:04:08 +0900

    Update license

--- bin/test_nodejs_server.js
+++ bin/test_nodejs_server.js
@@ -1,3 +1,19 @@
+// Webtable is to show tabular data in plot form as well as table.
+// Copyright (C) 2015  Hyungchan Kim <inlinechan@gmail.com>
+//
+// This program is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
 var tail = require('tail').Tail;
 var ws = require('nodejs-websocket');
 var lineReader = require('line-reader');
